ce9ee249ad898ba3506a1f36fabe63b0a5057338
Ajeywatpade <ajey.archent@gmail.com>
2025-06-13 15:22:05 +0530

    Update cors.js

--- config/cors.js
+++ config/cors.js
@@ -16,7 +16,7 @@ module.exports = {
   | Function - Receives the current origin and should return one of the above values.
   |
   */
-  origin: ['https://login.archenterprises.co.in'],
+  origin: true,
 
   /*
   |--------------------------------------------------------------------------
