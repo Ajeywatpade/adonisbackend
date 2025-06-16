a2a79a2fa24378675a9817cea33bfba6ff3a5f31
Ajeywatpade <ajey.archent@gmail.com>
2025-06-16 09:54:58 +0530

    Update AttendanceController.js

--- app/Controllers/Http/AttendanceController.js
+++ app/Controllers/Http/AttendanceController.js
@@ -1,1 +1,26 @@
+'use strict'
+
+const Attendance = use('App/Models/Attendance')
+
+class AttendanceController {
+  async store({ request, response }) {
+    const { attendances, date } = request.all()
+
+    try {
+      const insertData = attendances.map(item => ({
+        employee_id: item.employee_id,
+        status: item.status,
+        date: date
+      }))
+
+      await Attendance.createMany(insertData)
+
+      return response.status(201).json({ success: true, message: 'Attendance saved.' })
+    } catch (err) {
+      return response.status(500).json({ success: false, message: err.message })
+    }
+  }
+}
+
+module.exports = AttendanceController
 
